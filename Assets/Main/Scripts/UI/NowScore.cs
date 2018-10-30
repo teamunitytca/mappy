48b48968d1d64cdb3d40c5b22496e82c744055e6
ou <kamijoukyo@gmail.com>
2018-10-30 16:03:19 +0900

    スコア加算の実装 トップスコアの実装

--- Assets/Main/Scripts/UI/NowScore.cs
+++ Assets/Main/Scripts/UI/NowScore.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 
 public class NowScore : MonoBehaviour {
-	[SerializeField] int _now_score = 0;
+	int _now_score = 0;
 	[SerializeField] Text _text = null;
 
 	// Use this for initialization
@@ -13,6 +13,13 @@ public class NowScore : MonoBehaviour {
 	// Update is called once per frame
 	void Update ( ) {
 		_text.text = "NOW SCORE" + "\n" + _now_score.ToString( "D2" );
+	}
 
+	public void addScore ( int score ) {
+		_now_score += score;
+	}
+
+	public int getScore ( ) {
+		return _now_score;
 	}
 }
