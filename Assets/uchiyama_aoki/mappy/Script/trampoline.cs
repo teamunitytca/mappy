2ec014ed40c33fa0ea259c5e65f4271a1a09f149
内山 <tcagame@tca.ac.jp>
2018-11-15 15:33:07 +0900

    トランポリンの色が変わっていくようにした。

---
+++ Assets/uchiyama_aoki/mappy/Script/trampoline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trampoline : MonoBehaviour {
+
+	private Animator animator = null;
+	public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start ( ) {
+		animator = GetComponent< Animator >( );
+	}
+	
+	// Update is called once per frame
+	void Update ( ) {
+
+	}
+	void OnCollisionEnter2D( Collision2D collision ){
+		if ( collision.gameObject.tag == "Player" ) {
+			Debug.Log ( "まっぴー" );
+			animator.SetTrigger ( "Jump" );
+			player.velocity = new Vector2 ( 0, 10 );
+		}
+	}	
+}
