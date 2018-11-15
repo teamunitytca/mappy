631b8bb7ff15640a7265e29953466e2b3e44b658
清野 <tcagame@tca.ac.jp>
2018-11-15 15:37:20 +0900

    パワードアの色変更、パワードアの開閉、マイクロウェーブの移動
    Signed-off-by: 清野 <tcagame@tca.ac.jp>

---
+++ Assets/kiyono/script/doormotion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doormotion : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionStay2D(Collision2D collision) {
+
+		if (collision.gameObject.tag == "Player") {
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				GameObject prefab = (GameObject)Resources.Load ("kiyono/Resources/microwave");
+
+				GetComponent<Animator> ().SetTrigger("trigger");
+
+					Debug.Log ("(・∀・)ｲｲ!!");
+			}
+		}
+	}
+
+}
