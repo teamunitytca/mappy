376a20564c4e988e7e90402b74eb8ca31acbf594
TANG <tangtungming@gmail.com>
2018-12-06 15:49:22 +0900

    ENEMY 移動
    Signed-off-by: TANG <tangtungming@gmail.com>

---
+++ Assets/TANG/DOOR ACTION/COLLIDER.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COLLIDER : MonoBehaviour {
+	private Animator door = null;
+	void Start () {
+		door = GetComponent<Animator> ();
+	}
+	private void OnCollisionEnter2D(Collision2D col)
+	{if (col.gameObject.name == "Mappy_OBJ_enemy_0") {
+			door.SetBool ("event", false);
+		}
+	}
+}
