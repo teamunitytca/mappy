f6db63912dff9bbcf84db06849c6223b943f2a29
ou <kamijoukyo@gmail.com>
2018-12-13 13:25:30 +0900

    プレイヤー修正

--- Assets/Main/Scripts/Player/Player.cs
+++ Assets/Main/Scripts/Player/Player.cs
@@ -2,20 +2,20 @@
 
 public class Player : MonoBehaviour {
 	float _move_input = 0;
-	[ SerializeField] bool _movable = true;
+	[SerializeField] bool _movable = true;
 	[SerializeField] float _speed = 0;
 	[SerializeField] GameObject _startPos = null;
 	Rigidbody2D _rb;
 	LifeCounter _life;
 
 	// Use this for initialization
-	void Start ( ) {
+	void Start( ) {
 		_rb = GetComponent<Rigidbody2D>( );
 		_life = GameObject.Find( "LifeCounter" ).GetComponent<LifeCounter>( );
 	}
 
 	// Update is called once per frame
-	void Update ( ) {
+	void Update( ) {
 		if ( _movable ) {
 			move( );
 		}
@@ -25,19 +25,19 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	void move ( ) {
+	void move( ) {
 		_move_input = Input.GetAxisRaw( "Horizontal" );
 		transform.position += new Vector3( _move_input * _speed, 0, 0 ) * Time.deltaTime;
 		if ( _move_input < 0 ) {
-			transform.localScale = new Vector3( 5, 5, 1 );
+			transform.localScale = new Vector3( Mathf.Abs( transform.localScale.x ), transform.localScale.y, transform.localScale.z );
 		}
 		if ( _move_input > 0 ) {
-			transform.localScale = new Vector3( -5, 5, 1 );
+			transform.localScale = new Vector3( -Mathf.Abs( transform.localScale.x ), transform.localScale.y, transform.localScale.z );
 		}
 
 	}
 
-	void jump ( ) {
+	void jump( ) {
 		_movable = false;
 		if ( _move_input < 0 ) {
 			_rb.velocity = new Vector2( -50, 250 ) * Time.deltaTime;
@@ -47,11 +47,11 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	void resetPos ( ) {
+	void resetPos( ) {
 		transform.position = _startPos.transform.position;
 	}
 
-	void OnCollisionEnter2D ( Collision2D collision ) {
+	void OnCollisionEnter2D( Collision2D collision ) {
 		if ( collision.gameObject.tag == "Enemy" ) {
 			_life.loseLife( );
 			resetPos( );
@@ -59,7 +59,7 @@ public class Player : MonoBehaviour {
 		_movable = true;
 	}
 
-	void OnCollisionExit2D ( Collision2D collision ) {
+	void OnCollisionExit2D( Collision2D collision ) {
 		if ( collision.gameObject.tag == "Floor" ) {
 			jump( );
 		}
