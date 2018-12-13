93e6a5e6f810d8be11d01240e0064d852178795a
LAPTOP-04GF6155\Xenose <Xenose@live.com>
2018-12-13 12:55:40 +0900

    ->prefab added *canvas prefab *tranpoline *house *microWave
    ->script added
    *MicroWave
    *CamraTracker
    
    ->Scene added
    *house Scene

---
+++ Assets/Entity-seb/Script/CamraTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraTracker : MonoBehaviour
+{
+    [SerializeField]
+    private float _posZ;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        cam.transform.position = new Vector3(0, 0, _posZ) + (Vector3)((Vector2)this.gameObject.transform.position);  
+    }
+}
---
+++ Assets/Entity-seb/Script/MicroWave.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicroWave : MonoBehaviour
+{
+    public bool _isFacingLeft = true;
+    [SerializeField]
+    private bool _hitWall = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall")
+            _hitWall = true;
+
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<Meowkie>().enabled = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && !_hitWall)
+        {
+            collision.gameObject.transform.position = new Vector3(
+                (collision.transform.position.x + transform.position.x) * 0.5f, 
+                collision.transform.position.y, 
+                collision.transform.position.z
+                );
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<Meowkie>().enabled = true;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+		if (_isFacingLeft)
+        {
+            this.gameObject.transform.position += new Vector3(-.01f, 0, 0);
+            this.gameObject.transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else
+        {
+            this.gameObject.transform.position += new Vector3(.01f, 0, 0);
+            this.gameObject.transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+	}
+}
