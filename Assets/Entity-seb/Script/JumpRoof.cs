95f367d303dd883da3f2e51c39be9d08e53ede1e
LAPTOP-04GF6155\Xenose <Xenose@live.com>
2018-12-20 14:10:41 +0900

    fixed JumpRoof

--- Assets/Entity-seb/Script/JumpRoof.cs
+++ Assets/Entity-seb/Script/JumpRoof.cs
@@ -6,6 +6,9 @@ public class JumpRoof : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.gameObject.GetComponent<Entity>().SetState(Entity.MOVING.IDLE);
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Entity>().SetState(Entity.MOVING.IDLE);
+        }
     }
 }
