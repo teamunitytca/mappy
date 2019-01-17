78df916f0b6ce88877bc8256d79d14f1c8688bba
TANG <tangtungming@gmail.com>
2019-01-17 15:48:49 +0900

    darapoの判定

--- Assets/TANG/Dorapo/step.cs
+++ Assets/TANG/Dorapo/step.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class step : MonoBehaviour {
-	
+	private Animator _animator = null;
+
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
 	void OnCollisionExit2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
-			Destroy( this.gameObject );
+			_animator.SetTrigger ("trigger");
 		}
 	}
 }
