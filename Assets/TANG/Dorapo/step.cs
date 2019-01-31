ea0e6b228d44cc8b0f34487fec64a9a9dfbff63e
TANG <tangtungming@gmail.com>
2019-01-31 09:59:14 +0900

    落ちし穴判定

--- Assets/TANG/Dorapo/step.cs
+++ Assets/TANG/Dorapo/step.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class step : MonoBehaviour {
 	private Animator _animator = null;
-
+	bool _counter = false;
+	[SerializeField] float countdown = 3;
 	void Start () {
 		_animator = GetComponent<Animator> ();
 	}
 	void OnCollisionExit2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
-			_animator.SetTrigger ("trigger");
+			_animator.SetBool ("return", true);
+			_animator.SetTrigger ("GG");
+			_counter = true;
 		}
+	}
+	void Update(){
+		if (_counter == true) {
+			countdown -= Time.deltaTime;
+			if (countdown <= 0) {
+				_counter = false;
+				countdown = 3;
+				_animator.SetBool ("return", false);
+			}
+		}
+	
 	}
 }
