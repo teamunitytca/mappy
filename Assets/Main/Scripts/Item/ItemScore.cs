972bbb060040a641c409a5ccc978c3150d4e58e4
Sebastian <Xenose@live.com>
2019-01-31 14:11:16 +0900

    Hurry upの関数を半分を終わり

--- Assets/Main/Scripts/Item/ItemScore.cs
+++ Assets/Main/Scripts/Item/ItemScore.cs
@@ -2,46 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemScore : Item {
-	const int RADIO_SCORE         = 100;
-	const int TV_SCORE            = 200;
-	const int MICROCOMPUTER_SCORE = 300;
-	const int MONARISA_SCORE      = 400;
-	const int SAFE_SCORE          = 500;
+public class ItemScore : Item
+{
+    const int RADIO_SCORE = 100;
+    const int TV_SCORE = 200;
+    const int MICROCOMPUTER_SCORE = 300;
+    const int MONARISA_SCORE = 400;
+    const int SAFE_SCORE = 500;
 
-	NowScore _score;
-	AudioSource _audio;
+    NowScore _score;
+    AudioSource _audio;
 
-	// Use this for initialization
-	void Start ( ) {
-		_score = GameObject.Find( "NowScoreText" ).GetComponent<NowScore>( );
-		_audio = GameObject.Find( "Item_SE" ).GetComponent<AudioSource>( );
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _score = GameObject.Find("NowScoreText").GetComponent<NowScore>();
+        _audio = GameObject.Find("Item_SE").GetComponent<AudioSource>();
+    }
 
-	// Update is called once per frame
-	void Update ( ) {
-
-	}
-	void OnDestroy ( ) {
-		if ( _audio ) {
-			_audio.PlayOneShot( _audio.clip );
-		}
-		switch ( tag ) {
-			case "Radio":
-				_score.addScore( RADIO_SCORE );
-				break;
-			case "TV":
-				_score.addScore( TV_SCORE );
-				break;
-			case "MicroComputer":
-				_score.addScore( MICROCOMPUTER_SCORE );
-				break;
-			case "Monarisa":
-				_score.addScore( MONARISA_SCORE );
-				break;
-			case "Safe":
-				_score.addScore( SAFE_SCORE );
-				break;
-		}
-	}
+    void OnDestroy()
+    {
+        if (_audio)
+        {
+            _audio.PlayOneShot(_audio.clip);
+        }
+        switch (tag)
+        {
+            case "Radio":
+                _score.addScore(RADIO_SCORE);
+                break;
+            case "TV":
+                _score.addScore(TV_SCORE);
+                break;
+            case "MicroComputer":
+                _score.addScore(MICROCOMPUTER_SCORE);
+                break;
+            case "Monarisa":
+                _score.addScore(MONARISA_SCORE);
+                break;
+            case "Safe":
+                _score.addScore(SAFE_SCORE);
+                break;
+        }
+    }
 }
--- Assets/Main/Scripts/Item/ItemSpawn.cs
+++ Assets/Main/Scripts/Item/ItemSpawn.cs
@@ -2,60 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSpawn : MonoBehaviour {
+public class ItemSpawn : MonoBehaviour
+{
 
-	[SerializeField] GameObject [ ] _item = null;
+    [SerializeField] GameObject[] _item = null;
 
-	[SerializeField] GameObject [ ] _item_pos = null;
+    [SerializeField] GameObject[] _item_pos = null;
 
-	[SerializeField] GameObject [ ] _empty_pos = null;
+    [SerializeField] GameObject[] _empty_pos = null;
 
-	GameObject _hurryUp;
+    [SerializeField]
+    int _blinkingIntervel = 0;
+    GameObject _hurryUp;
 
-	int _item_no = 0;
+    int _item_no = 0;
 
-	// Use this for initialization
-	void Start( ) {
-		_hurryUp = GameObject.FindWithTag( "HurryUpText" );
-		_hurryUp.SetActive( false );
+    // Use this for initialization
+    void Start()
+    {
+        _hurryUp = GameObject.FindWithTag("HurryUpText");
+        _hurryUp.SetActive(false);
 
-		_empty_pos = new GameObject [ transform.childCount ];
-		_item_pos = new GameObject [ transform.childCount ];
+        _empty_pos = new GameObject[transform.childCount];
+        _item_pos = new GameObject[transform.childCount];
 
-		for ( int i = 0; i < transform.childCount; i++ ) {
-			_empty_pos [ i ] = transform.GetChild( i ).gameObject;
-		}
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _empty_pos[i] = transform.GetChild(i).gameObject;
+        }
 
-		itemSet( );
-	}
+        itemSet();
+    }
 
-	// Update is called once per frame
-	void Update( ) {
-		if ( !HasItem( ) ) {
-			_hurryUp.SetActive( true );
-		}
-	}
+    private void FixedUpdate()
+    {
+        if (!HasItem())
+        {
+            if (_blinkingIntervel > 10)
+            {
+                _hurryUp.SetActive(!_hurryUp.activeSelf);
+                _blinkingIntervel = 0;
+            }
+            else
+                _blinkingIntervel++;
+        }
+    }
 
-	void itemSet( ) {
-		for ( int i = 0; i < transform.childCount; i++ ) {
-			int empty_pos_no = Random.Range( 0, transform.childCount );
+    void itemSet()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            int empty_pos_no = Random.Range(0, transform.childCount);
 
-			while ( _item_pos [ empty_pos_no ] != null ) {
-				empty_pos_no = Random.Range( 0, transform.childCount );
-			}
-			_item_pos [ empty_pos_no ] = Instantiate( _item [ _item_no % _item.Length ], _empty_pos [ empty_pos_no ].transform.position, Quaternion.identity );
-		}
-		_item_no++;
-	}
+            while (_item_pos[empty_pos_no] != null)
+            {
+                empty_pos_no = Random.Range(0, transform.childCount);
+            }
+            _item_pos[empty_pos_no] = Instantiate(_item[_item_no % _item.Length], _empty_pos[empty_pos_no].transform.position, Quaternion.identity);
+        }
+        _item_no++;
+    }
 
-	bool HasItem( ) {
-		bool tmp = false;
+    bool HasItem()
+    {
+        bool tmp = false;
 
-		for ( int i = 0; i < _item_pos.Length; i++ ) {
-			if ( _item_pos [ i ] != null ) {
-				tmp = true;
-			}
-		}
-		return tmp;
-	}
+        for (int i = 0; i < _item_pos.Length; i++)
+        {
+            if (_item_pos[i] != null)
+            {
+                tmp = true;
+            }
+        }
+        return tmp;
+    }
 }
