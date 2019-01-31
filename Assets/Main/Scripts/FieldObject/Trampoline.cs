9c6d05cf53c3b62feca9b034e2649cbf01566378
ou <kamijoukyo@gmail.com>
2019-01-31 15:23:38 +0900

    アイテム生成修正

--- Assets/Main/Scripts/FieldObject/Trampoline.cs
+++ Assets/Main/Scripts/FieldObject/Trampoline.cs
@@ -1,70 +1,60 @@
 ﻿using UnityEngine;
 
-public class Trampoline : MonoBehaviour 
-{
+public class Trampoline : MonoBehaviour {
 	const int TRAMPOLINE_MAXHP = 4;
 	int _trampoline_HP = TRAMPOLINE_MAXHP;
 
 	bool _ontop;
-	Collider2D[] _col;
+	Collider2D [ ] _col;
 	Animator _trampoline_anim;
-	
-	[SerializeField] 
+
+	[SerializeField]
 	Vector2 _velocity = Vector2.zero;
 
 	[SerializeField] int _trampoline_no = 0;
 
 	// Use this for initialization
-	void Start() 
-	{
+	void Start( ) {
 		_ontop = false;
-		_col = GetComponents<Collider2D>();
-		_trampoline_anim = GetComponent<Animator>();
+		_col = GetComponents<Collider2D>( );
+		_trampoline_anim = GetComponent<Animator>( );
 		_trampoline_anim.SetInteger( "NO", _trampoline_no );
 	}
 
 	// Update is called once per frame
-	void Update() 
-	{
-		if ( _trampoline_HP <= 0 )
-        {
+	void Update( ) {
+		if ( _trampoline_HP <= 0 ) {
 			changeColor( );
-			_col [1].isTrigger = true;
+			_col [ 1 ].isTrigger = true;
 			return;
 		}
 
-		_col[1].isTrigger = false;
+		_col [ 1 ].isTrigger = false;
 	}
 
 	void changeColor( ) {
 		_trampoline_anim.SetInteger( "HP", _trampoline_HP );
 	}
 
-	void OnCollisionEnter2D(Collision2D collision) 
-	{
-		if ( _ontop && ( collision.gameObject.tag == "Player"  || collision.gameObject.tag  == "Enemy" ) )
-        {
+	void OnCollisionEnter2D( Collision2D collision ) {
+		if ( _ontop && ( collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy" ) ) {
 			_trampoline_anim.SetBool( "OnBound", true );
-            collision.gameObject.GetComponent<Entity>().SetState(Entity.MOVING.JUMP);
+			collision.gameObject.GetComponent<Entity>( ).SetState( Entity.MOVING.JUMP );
 		}
 	}
 
-	void OnCollisionStay2D(Collision2D collision)
-    {
-		if ( _ontop && ( collision.gameObject.tag == "Player"  || collision.gameObject.tag  == "Enemy" ) )
-        {
+	void OnCollisionStay2D( Collision2D collision ) {
+		if ( _ontop && ( collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy" ) ) {
 			_trampoline_anim.SetBool( "OnBound", true );
-            collision.gameObject.GetComponent<Entity>().SetState(Entity.MOVING.JUMP);
+			collision.gameObject.GetComponent<Entity>( ).SetState( Entity.MOVING.JUMP );
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D collision) 
-	{
+	void OnTriggerEnter2D( Collider2D collision ) {
 		_ontop = true;
 	}
 
-	void OnTriggerExit2D ( Collider2D collision )
-    {
+	void OnTriggerExit2D( Collider2D collision ) {
 		_ontop = false;
 		_trampoline_anim.SetBool( "OnBound", false );
 
@@ -73,8 +63,7 @@ public class Trampoline : MonoBehaviour
 		}
 	}
 
-	public void resetTrampolineHP ( )
-    {
+	public void resetTrampolineHP( ) {
 		_trampoline_HP = TRAMPOLINE_MAXHP;
 	}
 }
--- Assets/Main/Scripts/Item/ItemSpawn.cs
+++ Assets/Main/Scripts/Item/ItemSpawn.cs
@@ -2,79 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemSpawn : MonoBehaviour
-{
+public class ItemSpawn : MonoBehaviour {
 
-    [SerializeField] GameObject[] _item = null;
+	[SerializeField] GameObject [ ] _item = null;
 
-    [SerializeField] GameObject[] _item_pos = null;
+	[SerializeField] GameObject [ ] _item_pos = null;
 
-    [SerializeField] GameObject[] _empty_pos = null;
+	[SerializeField] GameObject [ ] _empty_pos = null;
 
-    [SerializeField]
-    int _blinkingIntervel = 0;
-    GameObject _hurryUp;
-    
-    int _item_no = 0;
-    const int APPEAR_LIMIT = 14;
+	[SerializeField]
+	int _blinkingIntervel = 0;
+	GameObject _hurryUp;
 
-    // Use this for initialization
-    void Start()
-    {
-        _hurryUp = GameObject.FindWithTag("HurryUpText");
-        _hurryUp.SetActive(false);
+	int _item_no = 0;
+	const int APPEAR_LIMIT = 14;
 
-        _empty_pos = new GameObject[transform.childCount];
-        _item_pos = new GameObject[transform.childCount];
+	// Use this for initialization
+	void Start( ) {
+		_hurryUp = GameObject.FindWithTag( "HurryUpText" );
+		_hurryUp.SetActive( false );
 
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            _empty_pos[i] = transform.GetChild(i).gameObject;
-        }
+		_empty_pos = new GameObject [ transform.childCount ];
+		_item_pos = new GameObject [ transform.childCount ];
 
-        itemSet();
-    }
+		for ( int i = 0; i < transform.childCount; i++ ) {
+			_empty_pos [ i ] = transform.GetChild( i ).gameObject;
+		}
 
-    private void FixedUpdate()
-    {
-        if (!HasItem())
-        {
-            if (_blinkingIntervel > 10)
-            {
-                _hurryUp.SetActive(!_hurryUp.activeSelf);
-                _blinkingIntervel = 0;
-            }
-            else
-                _blinkingIntervel++;
-        }
-    }
-    
-    void itemSet()
-    {
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            int empty_pos_no = Random.Range(0, transform.childCount);
+		itemSet( );
+	}
 
-            while (_item_pos[empty_pos_no] != null)
-            {
-                empty_pos_no = Random.Range(0, transform.childCount);
-            }
-            _item_pos[empty_pos_no] = Instantiate(_item[_item_no % _item.Length], _empty_pos[empty_pos_no].transform.position, Quaternion.identity);
-        }
-        _item_no++;
-    }
+	private void FixedUpdate( ) {
+		if ( !HasItem( ) ) {
+			if ( _blinkingIntervel > 10 ) {
+				_hurryUp.SetActive( !_hurryUp.activeSelf );
+				_blinkingIntervel = 0;
+			} else
+				_blinkingIntervel++;
+		}
+	}
 
-    bool HasItem()
-    {
-        bool tmp = false;
+	void itemSet( ) {
+		if ( _item_pos.Length < APPEAR_LIMIT ) {
+			return;
+		}
+		for ( int i = 0; i < APPEAR_LIMIT; i++ ) {
+			int empty_pos_no = Random.Range( 0, transform.childCount );
 
-        for (int i = 0; i < _item_pos.Length; i++)
-        {
-            if (_item_pos[i] != null)
-            {
-                tmp = true;
-            }
-        }
-        return tmp;
-    }
+			while ( _item_pos [ empty_pos_no ] != null ) {
+				empty_pos_no = Random.Range( 0, transform.childCount );
+			}
+
+			_item_pos [ empty_pos_no ] = Instantiate( _item [ _item_no % _item.Length ], _empty_pos [ empty_pos_no ].transform.position, Quaternion.identity );
+			_item_no++;
+		}
+	}
+
+	bool HasItem( ) {
+		bool tmp = false;
+
+		for ( int i = 0; i < _item_pos.Length; i++ ) {
+			if ( _item_pos [ i ] != null ) {
+				tmp = true;
+			}
+		}
+		return tmp;
+	}
 }
