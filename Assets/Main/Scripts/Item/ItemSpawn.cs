a3b45989fa2c024800678b788a22b333f8905f89
ou <kamijoukyo@gmail.com>
2019-01-31 11:42:21 +0900

    スコアソート修正 アイテム生成修正

--- Assets/Main/Scripts/Item/ItemSpawn.cs
+++ Assets/Main/Scripts/Item/ItemSpawn.cs
@@ -7,7 +7,7 @@ public class ItemSpawn : MonoBehaviour {
 	[SerializeField] GameObject [ ] _item = null;
 	[SerializeField] GameObject [ ] _item_pos = null;
 	[SerializeField] GameObject [ ] _empty_pos = null;
-
+	const int appear_limit = 14;
 
 	// Use this for initialization
 	void Start( ) {
@@ -22,18 +22,18 @@ public class ItemSpawn : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update( ) {
-
 	}
+	int item_no = 0;
 
 	void itemSet( ) {
-
-		for ( int i = 0; i < transform.childCount; i++ ) {
-			int item_no = Random.Range( 0, _item.Length );
+		item_no = 0;
+		for ( int i = 0; i < appear_limit; i++ ) {
 			int empty_pos_no = Random.Range( 0, transform.childCount );
 			while ( _item_pos [ empty_pos_no ] != null ) {
 				empty_pos_no = Random.Range( 0, transform.childCount );
 			}
-			_item_pos [ empty_pos_no ] = Instantiate( _item [ item_no ], _empty_pos [ empty_pos_no ].transform.position, Quaternion.identity );
+			_item_pos [ empty_pos_no ] = Instantiate( _item [ item_no % _item.Length ], _empty_pos [ empty_pos_no ].transform.position, Quaternion.identity );
+			item_no++;
 		}
 	}
 }
